56173480086cca14f77aafe834febdfee468142d
woojin <kevin2205@naver.com>
2023-07-10 16:09:55 +0900

    서브추가

--- drg-app/src/pages/cleansingData.js
+++ drg-app/src/pages/cleansingData.js
@@ -1,86 +1,115 @@
-const cleansing= [
-    {
-      id: 'cleansing01',
-      image: process.env.PUBLIC_URL+'/subimg/cleansing/cleansing01.jpg',
-      desc: '가볍고 신축성이 좋으며 우수한 터치감과 통기성으로 땀배출이 좋은 잠재권축사 소재의 티셔츠입니다.',
-      title: '썸머 우븐 포켓 반바지',
-      tag : '#남성 #라이프스타일',
-      price: 69000
-    },
-    {
-        id: 'cleansing02',
-        image: process.env.PUBLIC_URL+'/subimg/cleansing/cleansing02.jpg',
-        desc: '가볍고 신축성이 좋으며 우수한 터치감과 통기성으로 땀배출이 좋은 잠재권축사 소재의 티셔츠입니다.',
-        title: '썸머 시어서커 우븐 반바지',
-        tag : '#공용 #라이프스타일',
-        price: 69000
-      },
-      {
-        id: 'cleansing03',
-        image: process.env.PUBLIC_URL+'/subimg/cleansing/cleansing03.jpg',
-        desc: '부드러운 터치감과 높은 퀄리티의 면/폴리에스터 이중지 소재입니다.',
-        title: '모노그램 우븐 반바지',
-        tag : '#공용 #레귤러핏 #라이프스타일',
-        price: 79000
-      },
-      {
-        id: 'cleansing04',
-        image: process.env.PUBLIC_URL+'/subimg/cleansing/cleansing04.jpg',
-        desc: '부드러운 터치감과 높은 퀄리티의 면/폴리에스터 이중지 소재입니다.',
-        title: '컴포트핏 6부 반바지',
-        tag : '#공용 #컴포트핏 #트레이닝',
-        price: 59000
-      },
-      {
-        id: 'cleansing05',
-        image: process.env.PUBLIC_URL+'/subimg/cleansing/cleansing05.jpg',
-        desc: '부드러운 터치감과 높은 퀄리티의 면/폴리에스터 이중지 소재입니다.',
-        title: '테잎 오버핏 우븐 반바지',
-        tag : '#공용 #라이프스타일',
-        price: 79000
-      },
-      {
-        id: 'cleansing06',
-        image: process.env.PUBLIC_URL+'/subimg/cleansing/cleansing06.jpg',
-        desc: '부드러운 터치감과 높은 퀄리티의 면/폴리에스터 이중지 소재입니다.',
-        title: '우븐 조거팬츠',
-        tag : '#공용 #컴포트핏 ',
-        price: 109000
-      },
-      {
-        id: 'cleansing07',
-        image: process.env.PUBLIC_URL+'/subimg/cleansing/cleansing07.jpg',
-        desc: '우수한 형태 안정성이 특징인 면/폴리에스터 이중지 소재를 사용했습니다.',
-        title: '우븐 조거팬츠',
-        tag : '#공용 #컴포트핏 ',
-        price: 109000
-      },
-      {
-        id: 'cleansing08',
-        image: process.env.PUBLIC_URL+'/subimg/cleansing/cleansing08.jpg',
-        desc: '우수한 형태 안정성이 특징인 면/폴리에스터 이중지 소재를 사용했습니다.',
-        title: '이지팬츠',
-        tag : '#공용 #오버핏',
-        price: 99000
-      },
-      {
-        id: 'cleansing09',
-        image: process.env.PUBLIC_URL+'/subimg/cleansing/cleansing09.jpg',
-        desc: '우수한 형태 안정성이 특징인 면/폴리에스터 이중지 소재를 사용했습니다.',
-        title: '이지팬츠',
-        tag : '#공용 #오버핏',
-        price: 99000
-      },
-      {
-        id: 'cleansing10',
-        image: process.env.PUBLIC_URL+'/subimg/cleansing/cleansing10.jpg',
-        desc: '흡습속건 및 컬러 표현이 우수한 잠재권축사를 사용한 그래픽 반팔티입니다.',
-        title: '스트링 와이드 팬츠',
-        tag : '#공용 #오버핏',
-        price: 129000
-      },
-   
-    
-  ]
+const cleansing = [{
+    id: 'cleansing01',
+    image: process.env.PUBLIC_URL + '/subimg/cleansing/cleansing01.jpg',
+    desc: '피지 잡는 수분 진정 클렌징',
+    title: '레드 블레미쉬 클리어 수딩 폼',
+    tag1: '#약산성',
+    tag2: '#클렌징',
+    tag3: '#저자극',
+    category: '클렌징',
+    price: 22000
+  },
+  {
+    id: 'cleansing02',
+    image: process.env.PUBLIC_URL + '/subimg/cleansing/cleansing02.jpg',
+    desc: '건조함 없이 모공 속까지 깔끔하게 딥 클렌징 해주는 슈퍼 시카 클렌징 폼',
+    title: '레드블레미쉬 시카 클렌징폼',
+    tag1: '#약산성',
+    tag2: '#클렌징',
+    tag3: '#저자극',
+    category: '클렌징',
+    price: 15000
+  },
+  {
+    id: 'cleansing03',
+    image: process.env.PUBLIC_URL + '/subimg/cleansing/cleansing03.jpg',
+    desc: '남자 피부 고민 말끔하게! 진정 수분 클렌징폼',
+    title: '레드 블레미쉬 포 맨 젠틀 클렌징폼',
+    tag1: '#약산성',
+    tag2: '#클렌징',
+    tag3: '#저자극',
+    category: '클렌징',
+    price: 20000
+  },
+  {
+    id: 'cleansing04',
+    image: process.env.PUBLIC_URL + '/subimg/cleansing/cleansing04.jpg',
+    desc: '수분 담은 산뜻한 오일로 피부가 편안한 클렌징 오일',
+    title: '약산성 클렌징 오일',
+    tag1: '#약산성',
+    tag2: '#클렌징',
+    tag3: '#저자극',
+    category: '클렌징',
+    price: 27000
+  },
+  {
+    id: 'cleansing05',
+    image: process.env.PUBLIC_URL + '/subimg/cleansing/cleansing05.jpg',
+    desc: '모공 속 노폐물까지 순하게 지워주는 촉촉한 클렌징 워터',
+    title: '약산성 클렌징 워터',
+    tag1: '#약산성',
+    tag2: '#클렌징',
+    tag3: '#저자극',
+    category: '클렌징',
+    price: 25000
+  },
+  {
+    id: 'cleansing06',
+    image: process.env.PUBLIC_URL + '/subimg/cleansing/cleansing06.jpg',
+    desc: '메이크업, 블랙헤드, 미세먼지 원스텝 딥 클렌징',
+    title: '그린 딥 클렌징 오일',
+    tag1: '#약산성',
+    tag2: '#클렌징',
+    tag3: '#저자극',
+    category: '클렌징',
+    price: 24000
+  },
+  {
+    id: 'cleansing07',
+    image: process.env.PUBLIC_URL + '/subimg/cleansing/cleansing07.jpg',
+    desc: '늘어진 모공을 쫀쫀하게 당겨주는 저자극 밀싹 딥 클렌징 폼',
+    title: '그린 딥 포어 클리어 폼',
+    tag1: '#약산성',
+    tag2: '#클렌징',
+    tag3: '#저자극',
+    category: '클렌징',
+    price: 22000
+  },
+  {
+    id: 'cleansing08',
+    image: process.env.PUBLIC_URL + '/subimg/cleansing/cleansing08.jpg',
+    desc: '촉촉하게 여드름성 피부를 완화하는 저자극 트러블 버블폼',
+    title: '에이클리어 밸런싱 버블 폼',
+    tag1: '#약산성',
+    tag2: '#클렌징',
+    tag3: '#저자극',
+    category: '클렌징',
+    price: 25000
+  },
+  {
+    id: 'cleansing09',
+    image: process.env.PUBLIC_URL + '/subimg/cleansing/cleansing09.jpg',
+    desc: '촉촉한 보습 클렌저',
+    title: '그린마인드 비건 젤 클렌저',
+    tag1: '#약산성',
+    tag2: '#클렌징',
+    tag3: '#저자극',
+    category: '클렌징',
+    price: 28000
+  },
+  {
+    id: 'cleansing10',
+    image: process.env.PUBLIC_URL + '/subimg/cleansing/cleansing10.jpg',
+    desc: '풍부한 영양의 마유와 EGF 성분이 함유된 고보습 클렌징 폼',
+    title: '프레스티지 마유 클렌징폼',
+    tag1: '#약산성',
+    tag2: '#클렌징',
+    tag3: '#저자극',
+    category: '클렌징',
+    price: 40000
+  },
+
+
+]
 
 export default cleansing;
